1ed17976afbc3c2bd924c94e994f143b36e3e41f
kaija <kaija.chang@gmail.com>
2017-06-15 00:22:07 +0800

    lookup all record and find detail log path

--- lib/fluent/plugin/filter_modsecurity.rb
+++ lib/fluent/plugin/filter_modsecurity.rb
@@ -3,15 +3,29 @@ require 'json'
 
 class Fluent::ModsecurityFilter < Fluent::Filter
     Fluent::Plugin.register_filter('modsecurity', self)
+
+    desc "Path prefix of the detail log"
+    config_param :path_prefix, :string, default: "/var/log"
+
     def configure(conf)
         super
+        @path_prefix = conf['path_prefix']
     end
+
     def filter(tag, time, record)
-        print record['message']
-        message = record['message']
-        list = message.split(" ")
-        file = File.read(list[5])
+        log_path = ""
+        record.each{ |key, value|
+            token = value.split(" ")
+            token.each { |v|
+                if v.start_with?(@path_prefix)
+                    log_path = v
+                    break
+                end
+            }
+        }
+        file = File.read(log_path)
         data_hash = JSON.parse(file)
-        data_hash
+        record['detail'] = data_hash
+        record
     end
 end
