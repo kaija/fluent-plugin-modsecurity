4f5f57823bf5bae130fee97a226833005518ea30
kaija <kaija.chang@gmail.com>
2017-06-14 23:39:53 +0800

    roughly split the message and read json file from record

--- lib/fluent/plugin/filter_modsecurity.rb
+++ lib/fluent/plugin/filter_modsecurity.rb
@@ -2,11 +2,16 @@
 require 'json'
 
 class Fluent::ModsecurityFilter < Fluent::Filter
-	Fluent::Plugin.register_filter('modsecurity', self)
-	def configure(conf)
-		super
-	end
-	def filter(tag, time, record)
-		print record
-	end
+    Fluent::Plugin.register_filter('modsecurity', self)
+    def configure(conf)
+        super
+    end
+    def filter(tag, time, record)
+        print record['message']
+        message = record['message']
+        list = message.split(" ")
+        file = File.read(list[5])
+        data_hash = JSON.parse(file)
+        data_hash
+    end
 end
